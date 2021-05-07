566e93ffa6a22ac51cb07325fa37609c6d5223b0
Võ Văn Nghĩa <vovannghia2409@gmail.com>
2021-05-07 21:24:50 +0200

    rest: add email verification

--- app.py
+++ app.py
@@ -1,15 +1,27 @@
 import json
 import os
+import re
 
 import psycopg2
 from flask import Flask, request
-from werkzeug.exceptions import HTTPException
+from werkzeug.exceptions import HTTPException, BadRequest
 from werkzeug.middleware.dispatcher import DispatcherMiddleware
 
 DATABASE_URL = os.environ.get("DATABASE_URL")
 SCHEMA_NAME = os.environ.get("SCHEMA_NAME")
 TABLE_NAME = os.environ.get("TABLE_NAME", "users")
 conn = psycopg2.connect(DATABASE_URL, options=f"-c search_path={SCHEMA_NAME}")
+
+
+check_email_regex = re.compile(
+    r"""(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])"""
+)
+
+
+def check_email(email):
+    if not check_email_regex.match(email):
+        raise BadRequest(f"{email} is not a valid email")
+
 
 root = Flask(__name__)
 
@@ -31,6 +43,7 @@ def create_user():
     with conn:
         with conn.cursor() as curs:
             user = request.get_json()
+            check_email(user["email"])
             curs.execute(
                 f"INSERT INTO {TABLE_NAME} (name, email) VALUES (%s, %s) RETURNING id",
                 (user["name"], user["email"]),
@@ -56,6 +69,7 @@ def update_user(user_id):
     with conn:
         with conn.cursor() as curs:
             user = request.get_json()
+            check_email(user["email"])
             curs.execute(
                 f"UPDATE {TABLE_NAME} SET name = %s, email = %s WHERE id = %s",
                 (user["name"], user["email"], user_id),
